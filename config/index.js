ed8f79b30365662c9ec74f6bb4206b602787ba18
13568571860 <304406616@qq.com>
2018-09-04 09:00:31 +0800

    end

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: './',
+    assetsPublicPath: '/',
     proxyTable: {
       '/v1': {
         target: 'https://elm.cangdu.org',
